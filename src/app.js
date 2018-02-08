02a4b2fffc0b0eeb1bd54595430a5d92df7807aa
Leo Q <leoq91@gmail.com>
2018-02-07 19:16:35 -0800

    CORS changes

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ app.use(logger('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
+  const allowed_header = [
+    'http://localhost:4000',
+    'www.gutsy.cc',
+    'http://gutsy.cc/',
+  ];
+  const origin = req.headers.origin;
+  if (allowed_header.indexOf(origin) > -1) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, gutsyJwt, Accept'
